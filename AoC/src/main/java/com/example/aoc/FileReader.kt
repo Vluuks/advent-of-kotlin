edc4989f35adb5e402325fcb9f011c3f8b5ba953
Renske <vluuks@gmail.com>
2022-06-16 15:21:49 +0200

    day1 huts life

--- AoC/src/main/java/com/example/aoc/FileReader.kt
+++ AoC/src/main/java/com/example/aoc/FileReader.kt
@@ -2,25 +2,16 @@ package com.example.aoc
 
 import java.io.File
 import java.io.InputStream
-import java.nio.file.Paths
 
 class FileReader(private val fileName: String) {
 
     fun readFile(): List<String> {
+        // crying emojis for not using relative path :'((((((
         val path = "/Users/renske/AndroidStudioProjects/AdventOfKode/AoC/src/main/java/com/example/aoc/day${fileName.last()}/${fileName}.txt"
         val inputStream: InputStream = File(path).inputStream()
         val lineList = mutableListOf<String>()
 
         inputStream.bufferedReader().forEachLine { lineList.add(it) }
-        lineList.forEach{println(">  " + it)}
-
         return lineList
     }
-}
-
-fun main() {
-
-
-
-
 }
--- AoC/src/main/java/com/example/aoc/day1/day1.kt
+++ AoC/src/main/java/com/example/aoc/day1/day1.kt
@@ -3,5 +3,40 @@ package com.example.aoc.day1
 import com.example.aoc.FileReader
 
 fun main() {
-    val lines: List<String> = FileReader("input1:").readFile()
+    part1()
+    part2()
+}
+
+fun part1() {
+    val lines: List<String> = FileReader("input1").readFile()
+
+    var floor = 0
+    for (c in lines[0]) {
+        if (c == '(') floor += 1
+        else if (c == ')') floor -= 1
+    }
+
+    print("\npart 1 \n")
+    print(floor)
+}
+
+fun part2() {
+    val lines: List<String> = FileReader("input1").readFile()
+    val characters = lines[0].toList()
+
+    var floor = 0
+    for (i in characters.indices) {
+        if (characters[i] == '(') {
+            floor += 1
+        }
+        else if (characters[i] == ')') {
+            floor -= 1
+
+            if (floor == -1) {
+                print("\npart 2 \n")
+                print(i+1)
+                break
+            }
+        }
+    }
 }
