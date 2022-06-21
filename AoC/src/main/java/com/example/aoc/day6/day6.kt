64bde5425df0b8b47cbd3492d412c0e88a842e79
Renske <vluuks@gmail.com>
2022-06-21 14:47:13 +0200

    i am so bad at this

--- AoC/src/main/java/com/example/aoc/day6/day6.kt
+++ AoC/src/main/java/com/example/aoc/day6/day6.kt
@@ -21,23 +21,47 @@ class Grid(private val rowCount: Int, private val columnCount: Int) {
         return grid
     }
 
-    fun alterLightState(command: String, row: Int, column: Int) {
+    private fun alterLightState(command: String, row: Int, column: Int) {
         when(command){
             "on" -> grid[row][column] = 1
             "off" -> grid[row][column] = 0
             "toggle" -> grid[row][column] = kotlin.math.abs(grid[row][column] - 1)
         }
     }
+
+    fun traverseGrid(topLeft: Pair<Int, Int>, bottomRight: Pair<Int, Int>, command: String) {
+        for(i in topLeft.first..bottomRight.first) {
+            println(i)
+            for(j in topLeft.second..bottomRight.second) {
+                println(j)
+                alterLightState(command, i, j)
+            }
+        }
+    }
+
+    fun print() {
+        for(row in grid){
+            println(row)
+        }
+    }
 }
-
-
 
 fun part1() {
     val lines: List<String> = FileReader("input6").readFile()
 
-    val grid = Grid(1000, 1000).initGrid()
+    val grid = Grid(5, 5)
+    grid.initGrid()
 
+
+    // turn on 887,9 through 959,629
     for(line in lines) {
-        // todo
+        val coordinates = Regex("(\\d+),(\\d+)").findAll(line).toList()
+        val topLeft = coordinates[0].groups[1]!!.value.toInt() to coordinates[0].groups[2]!!.value.toInt()
+        val bottomRight = coordinates[1].groups[1]!!.value.toInt() to coordinates[0].groups[2]!!.value.toInt()
+
+        grid.traverseGrid(topLeft, bottomRight, "on")
+        break
     }
+
+    grid.print()
 }
